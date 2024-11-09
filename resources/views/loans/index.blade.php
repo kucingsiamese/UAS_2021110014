b9387cae1ba55c4fbea054e1010475b44b2ab504
Navanya Anggraeni <isabellaa3095@gmail.com>
2024-11-09 17:46:13 +0700

    index loan and web.php

---
+++ resources/views/loans/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h2>Loan Books</h2>
+    <form method="POST" action="{{ url('/loans') }}">
+        @csrf
+        <div class="form-group">
+            <label for="book">Select Book</label>
+            <select class="form-control" name="book_id" id="book">
+                @foreach ($books as $book)
+                    <option value="{{ $book->id }}">{{ $book->title }}</option>
+                @endforeach
+            </select>
+        </div>
+        <button type="submit" class="btn btn-primary mt-3">Borrow Book</button>
+    </form>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,11 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Route::middleware(['auth', 'checkuser'])->group(function () {
 Route::get('/loans', [LoanController::class, 'index']);
 Route::post('/loans', [LoanController::class, 'store']);
 Route::get('/returns', [ReturnController::class, 'index']);
 Route::post('/returns', [ReturnController::class, 'store']);
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
